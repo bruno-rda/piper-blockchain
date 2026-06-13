7b85fb1df473b2de686403fce76c92b87b128c18
bruno-rda <206255519+bruno-rda@users.noreply.github.com>
2026-06-13 00:16:33 -0600

    feat: add env examples

--- backend/app/config.py
+++ backend/app/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     unit_name: str = "Pips"
 
     model_config = SettingsConfigDict(
-        env_file="../.env", env_file_encoding="utf-8", extra="ignore"
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
 
 
